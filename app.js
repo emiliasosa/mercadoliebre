e810a710a2adedb40d3677fded93e2cf22a65f07
Emilia Sosa <esosa@mobydigital.com>
2023-11-08 18:01:37 -0300

    Commit mejorando cosas

--- app.js
+++ app.js
@@ -3,9 +3,8 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.listen(3000, ()=>{
-    console.log('Servidor funcionando');
-});
+const port = process.env.PORT  || 3000;
+app.listen(port, ()=>console.log(`Servidor corriendo en el puerto ${port}`))
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
